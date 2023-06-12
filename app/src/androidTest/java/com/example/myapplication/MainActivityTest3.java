2602582e7e498a9415adc3b20aa5912c30183ea4
afnan <afnanislam130@gmail.com>
2023-06-12 11:21:54 -0400

    Finished the JavaDoc comments and testcases

---
+++ app/src/androidTest/java/com/example/myapplication/MainActivityTest3.java
@@ -0,0 +1,126 @@
+package com.example.myapplication;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest3 {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest3() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.editTextText),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatEditText.perform(replaceText("A"), closeSoftKeyboard());
+
+        pressBack();
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.editTextText), withText("A"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatEditText2.perform(replaceText("Aa@"));
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.editTextText), withText("Aa@"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatEditText3.perform(closeSoftKeyboard());
+
+        pressBack();
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.button), withText("LOGIN"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.textView), withText("Missing a number\n"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        textView.check(matches(withText("Missing a number ")));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.textView), withText("Missing a number\n"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        textView2.check(matches(withText("Missing a number ")));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.myapplication;
 
+import static java.lang.Character.isDigit;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,8 +10,22 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+/**
+ * In this lab I am validating the password if it includes at least one uppercase character, one lowercase character
+ * , and  one special character
+ * @author Afnan Islam
+ * @version 1.1
+ */
 public class MainActivity extends AppCompatActivity {
 
+
+    /**
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     *
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,57 +35,86 @@ public class MainActivity extends AppCompatActivity {
         EditText et = findViewById(R.id.editTextText);
         Button btn = findViewById(R.id.button);
 
-        btn.setOnClickListener(clk ->{
+        btn.setOnClickListener(clk -> {
             String password = et.getText().toString();
 
+            boolean isPasswordComplex = checkPasswordComplexity(password);
 
-            checkPasswordComplexity(password);
+
+
 
         });
     }
 
     /**
+     * Check the complexity of the password.
      *
-     * @param password The String object that we are checking
+     * @param password The password string to check
+     * @return True if the password meets complexity requirements, false otherwise
      */
     boolean checkPasswordComplexity(String password) {
-         boolean foundUpperCase, foundLowerCase, foundNumber, foundSpecial;
+        /**
+         * checks for an upper case character
+         */
+        boolean foundUpperCase = false;
+        /**
+         * checks for an lower case character
+         */
+        boolean foundLowerCase = false;
+        /**
+         * checks for a digit
+         */
+        boolean foundNumber = false;
+        /**
+         * checks for a special character
+         */
+        boolean foundSpecial = false;
+        /**
+         * attribute to CATCH the text of the state for each character
+         */
+        StringBuilder toastText = new StringBuilder();
 
-         foundUpperCase = foundLowerCase = foundNumber = foundSpecial = false;
+        /**
+         * A for loop to iterate through each character
+         */
 
-        if(!foundUpperCase)
-        {
+        for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
 
-            Toast.makeText();
-            // Say that they are missing an upper case letter;
-
-            return false;
-
+            if (Character.isUpperCase(c)) {
+                foundUpperCase = true;
+            } else if (Character.isLowerCase(c)) {
+                foundLowerCase = true;
+            } else if (Character.isDigit(c)) {
+                foundNumber = true;
+            } else {
+                /**
+                 *  Assume any character that is not a letter or digit is a special character
+                 */
+                foundSpecial = true;
+            }
         }
 
-        else if( ! foundLowerCase)
-        {
-            Toast.makeText( ... ); // Say that they are missing a lower case letter;
-
-            return false;
-
+        if (!foundUpperCase) {
+            toastText.append("Missing an uppercase letter\n");
         }
-
-        else if( ! foundNumber) { ....}
-
-        else if(! foundSpecial) { ... }
-
-        else
-
-            return true; //only get here if they're all true
-
-
-
-
-
-
+        if (!foundLowerCase) {
+            toastText.append("Missing a lowercase letter\n");
+        }
+        if (!foundNumber) {
+            toastText.append("Missing a number\n");
+        }
+        if (!foundSpecial) {
+            toastText.append("Missing a special character\n");
+        }
+        TextView tv = findViewById(R.id.textView);
+        if (toastText.length() > 0) {
+            tv.setText(toastText.toString());
+            return false;
+        } else {
+            tv.setText("Password meets complexity requirements");
+            return true;
+        }
     }
 
-
 }
-
